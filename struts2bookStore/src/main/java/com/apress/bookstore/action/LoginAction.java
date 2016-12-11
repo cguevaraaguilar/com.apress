2922e785f61c99b3852b61d70bf706d8434704ca
cguevara_aguilar <cguevara_aguilar@192.168.1.66>
2016-12-11 12:44:48 -0600

    Creación de proyecto struts2bookstore

---
+++ struts2bookStore/src/main/java/com/apress/bookstore/action/LoginAction.java
@@ -0,0 +1,26 @@
+package com.apress.bookstore.action;
+
+/*
+ * Descripción:	Clase controladora que realiza las operaciones de autenticación del usuario.
+ * Autor:		Carlos Ernesto Guevara Aguilar.
+ * F. Creación:	11 de Diciembre de 2016.
+ * F. Cambio:	11 de Diciembre de 2016.
+ * Comentarios:	
+ */
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Clase controladora que realiza las operaciones de autenticación del usuario.
+ * @author cguevara_aguilar
+ *
+ */
+public class LoginAction extends ActionSupport {
+	
+	//#region Propiedades
+	
+	//#endregion
+	
+	
+
+} // public class LoginAction extends ActionSupport {
